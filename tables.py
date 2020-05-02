77efba64077f54ee5738faaf60f3e6b77f02e2df
lubaina123 <lubainamachinewala@gmail.com>
2020-05-02 16:29:06 +0530

    Add files via upload

--- tables.py
+++ tables.py
@@ -10,6 +10,7 @@ conn=mydb.cursor()
 
 
 
+
 conn.execute('''CREATE  TABLE IF NOT EXISTS admin_user1(
   `id` INT NOT NULL auto_increment ,
   `admin_name` VARCHAR(150) ,
@@ -80,19 +81,23 @@ conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('cucumber',0.65,3,0.11,15,0.28,16,3.63);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('greenvegetables',2.9,1,0.4,23,2.7,99,3.6);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('sprouts',3,3,0.2,30,3.4,95.1,6);''' )
-conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('palak',2.9,3,0.4,23,2.7,99,3.6);''' )
-conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('methi',23,2,6,323,33.5,176,58);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('spinach',2.9,3,0.4,23,2.7,99,3.6);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('fenugreek',23,2,6,323,33.5,176,58);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('curd',11,1,4.3,98,0.1,83,3.4);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('chicken',27,2,14,239,760,15,0);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('bread',9,2,3.2,250,3.6,20,49);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('chapati',5.84,2,1.55,170,2.03,21,32.5);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('chana',19,2,6,364,2.2,36,61);''' )
-conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('urad',25.21,2,1.64,341000,7.57,138,58.99);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('urad',25.21,2,1.64,341,7.57,138,58.99);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('moong',24,2,1.2,347,6.7,132,63);''' )
-conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('toor',22,2,1.5,343,5.2,130,63);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('dal',22,2,1.5,343,5.2,130,63);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('masoor',9,2,0.4,116,3.3,19,20);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('cauliflower',1.9,3,0.3,25,0.4,22,5);''' )
 conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('cabbage',1.3,3,0.1,25,0.5,40,6);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('cashew',5.17,1,12.43,157,1.89,10,8.56);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('almond',11,1,2,163,1,76.3,6.1);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('sugar',0,0,0,387,0,0,100);''' )
+conn.execute('''INSERT INTO FoodMenu(dish_name,protein,vitamin,fat,calories,iron,calcium,carb) VALUES('honey',0.3,0,0,304,2,0,82);''' )
 conn.execute('''INSERT INTO diet_info(uname,height,weight,anaemic,iron,diabetic,calcium,vitamin) VALUES('Tasneem',176,46,0,0,1,1,2);''')
 mydb.commit()
 mydb.close()
